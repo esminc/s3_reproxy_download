6b786165ce898b88b99b48e09e85442bcc89e717
takkanm <takkanm@gmail.com>
2017-03-07 15:58:16 +0900

    configuarable reproxy path

--- lib/s3_reproxy_download.rb
+++ lib/s3_reproxy_download.rb
@@ -3,11 +3,19 @@ require 'action_controller'
 require 'aws-sdk'
 
 module S3ReproxyDownload
+  def self.reproxy_path=(path)
+    @reproxy_path = path
+  end
+
+  def self.reproxy_path
+    @reproxy_path || '/reproxy'
+  end
+
   module Helper
     def send_s3_file(bucket_name, path, headers: {})
       s3_object = S3ReproxyDownload::S3Object.new(bucket_name, path)
 
-      response.headers['X-Accel-Redirect'] = '/reproxy'
+      response.headers['X-Accel-Redirect'] = S3ReproxyDownload.reproxy_path
       response.headers['X-Reproxy-URL'] = s3_object.presigned_url
       response.headers['Content-Disposition'] = "attachment; filename=\"#{s3_object.filename}\""
 
